e3670d7ac467e07a3e49761bf78cd40cb8d7388b
yzh <dljtlxr@126.com>
2020-11-05 15:00:17 +0800

    second_commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+# git 地址
+#https://github.com/ChenYingpeng/darknet2caffe.git
+# learnopencv
+# https://github.com/spmallick/learnopencv.git
+# yolov4
+# https://github.com/AlexeyAB/darknet.git
+# https://github.com/rinetd/nnie_mapper.git
+# # 部署框架
+# https://github.com/OAID/Tengine.git
